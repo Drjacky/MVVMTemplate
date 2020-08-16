0728aa03bc0f34f95243470e6c15394dfd616149
Dr.jacky <dr.jacky.2005@gmail.com>
2020-08-16 20:25:27 +0200

    Remove old viewmodel extension

--- presentation/src/main/kotlin/app/web/drjackycv/presentation/extension/Extensions.kt
+++ presentation/src/main/kotlin/app/web/drjackycv/presentation/extension/Extensions.kt
@@ -1,21 +1,8 @@
 package app.web.drjackycv.presentation.extension
 
-import androidx.fragment.app.Fragment
 import androidx.lifecycle.LifecycleOwner
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.Observer
-import androidx.lifecycle.ViewModelProvider
-import app.web.drjackycv.presentation.base.viewmodel.BaseViewModel
-
-inline fun <reified T : BaseViewModel> Fragment.viewModel(
-    factory: ViewModelProvider.Factory,
-    body: T.() -> Unit = {}
-): T {
-    val vm = ViewModelProvider(this, factory).get(T::class.java)
-    vm.body()
-
-    return vm
-}
 
 fun <T> LifecycleOwner.observe(liveData: LiveData<T>, action: (t: T) -> Unit) {
     liveData.observe(this, Observer { it?.let { t -> action(t) } })
