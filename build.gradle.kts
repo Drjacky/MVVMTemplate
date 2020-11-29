715a449eaf80674105e2312ecc42ef40d25001a3
Dr.jacky <dr.jacky.2005@gmail.com>
2020-11-29 21:00:08 +0000

    [WIP] Add detekt

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,7 @@
 plugins {
-    id("org.sonarqube") version "3.0"
+    detekt
+    id("org.sonarqube") version app.web.drjackycv.buildsrc.Depends.Versions.sonarqubeVersion
+    //id("io.gitlab.arturbosch.detekt") version app.web.drjackycv.buildsrc.Depends.Versions.detektVersion
 }
 
 buildscript {
@@ -19,6 +21,7 @@ buildscript {
         classpath(app.web.drjackycv.buildsrc.Depends.ClassPaths.navigation_safe_args_gradle_plugin)
         classpath(app.web.drjackycv.buildsrc.Depends.ClassPaths.hilt_android_gradle_plugin)
         classpath(app.web.drjackycv.buildsrc.Depends.ClassPaths.sonarqube_gradle_plugin)
+        //classpath(app.web.drjackycv.buildsrc.Depends.ClassPaths.detekt_gradle_plugin)
     }
 }
 
@@ -32,6 +35,28 @@ allprojects {
         maven("https://plugins.gradle.org/m2/")
     }
 }
+
+/*detekt {
+    failFast = true // fail build on any finding
+    buildUponDefaultConfig = true // preconfigure defaults
+    config = files("$projectDir/config/detekt/detekt.yml") // point to your custom config defining rules to run, overwriting default behavior
+//    baseline = file("$projectDir/config/baseline.xml") // a way of suppressing issues before introducing detekt
+
+    reports {
+        html.enabled = true // observe findings in your browser with structure and code snippets
+        html.destination = file("$projectDir/build/detekt/report.html")
+        xml.enabled = true // checkstyle like format mainly for integrations like Jenkins
+        xml.destination = file("$projectDir/build/detekt/report.xml")
+        txt.enabled = true // similar to the console output, contains issue signature to manually edit baseline files
+        txt.destination = file("$projectDir/build/detekt/report.txt")
+    }
+}
+
+tasks {
+    withType<Detekt> {
+        this.jvmTarget = JavaVersion.VERSION_1_8.toString()
+    }
+}*/
 
 tasks.register<Delete>("clean") {
     delete(rootProject.buildDir)
--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -3,5 +3,10 @@ plugins {
 }
 
 repositories {
+    gradlePluginPortal()
     jcenter()
+}
+
+dependencies {
+    implementation("io.gitlab.arturbosch.detekt:detekt-gradle-plugin:1.15.0-RC1")
 }
