d1e5a551945b65a822ddc257a3df41e31b59c4f9
Dr.jacky <dr.jacky.2005@gmail.com>
2020-12-02 20:20:04 +0000

    Prevent member name to be equal to its parent class

--- presentation/src/main/kotlin/app/web/drjackycv/presentation/base/adapter/CellTypes.kt
+++ presentation/src/main/kotlin/app/web/drjackycv/presentation/base/adapter/CellTypes.kt
@@ -2,25 +2,25 @@ package app.web.drjackycv.presentation.base.adapter
 
 class CellTypes<T, R>(vararg types: Cell<T, R>) {
 
-    private val cellTypes: ArrayList<Cell<T, R>> = ArrayList()
+    private val cellTypesList: ArrayList<Cell<T, R>> = ArrayList()
 
     init {
         types.forEach { addType(it) }
     }
 
     private fun addType(type: Cell<T, R>) {
-        cellTypes.add(type)
+        cellTypesList.add(type)
     }
 
     fun of(item: T?): Cell<T, R> {
-        for (cellType in cellTypes) {
+        for (cellType in cellTypesList) {
             if (cellType.belongsTo(item)) return cellType
         }
         throw NoSuchRecyclerItemTypeException()
     }
 
     fun of(viewType: Int): Cell<T, R> {
-        for (cellType in cellTypes) {
+        for (cellType in cellTypesList) {
             if (cellType.type() == viewType) return cellType
         }
         throw NoSuchRecyclerViewTypeException()
