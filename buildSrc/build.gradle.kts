c406d412ffb14108810eb42eeb450b64aad8817e
Dr.jacky <dr.jacky.2005@gmail.com>
2020-12-02 12:54:44 +0000

    Add detekt

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -6,10 +6,6 @@ repositories {
     jcenter()
     gradlePluginPortal()
 }
-/*
-kotlinDslPluginOptions {
-    experimentalWarning.set(false)
-}*/
 
 dependencies {
     implementation("io.gitlab.arturbosch.detekt:detekt-gradle-plugin:1.15.0-RC1")
--- buildSrc/src/main/kotlin/app/web/drjackycv/buildsrc/detekt.gradle.kts
+++ buildSrc/src/main/kotlin/app/web/drjackycv/buildsrc/detekt.gradle.kts
@@ -1,16 +1,32 @@
-/*import io.gitlab.arturbosch.detekt.Detekt
+import io.gitlab.arturbosch.detekt.Detekt
 import io.gitlab.arturbosch.detekt.DetektCreateBaselineTask
-import io.gitlab.arturbosch.detekt.extensions.DetektExtension*/
-
-
+import io.gitlab.arturbosch.detekt.extensions.DetektExtension
 
 plugins {
     id("io.gitlab.arturbosch.detekt")
 }
-detekt {
-    toolVersion = "1.15.0-RC1"
-    config = files("config/detekt/detekt.yml")
 
+val analysisDir = file(projectDir)
+val baselineFile = file("$rootDir/config/detekt/baseline.xml")
+val configFile = file("$rootDir/config/detekt/detekt.yml")
+//val statisticsConfigFile = file("$rootDir/config/detekt/statistics.yml")
+
+val kotlinFiles = "**/*.kt"
+val kotlinScriptFiles = "**/*.kts"
+val resourceFiles = "**/resources/**"
+val buildFiles = "**/build/**"
+
+detekt {
+    toolVersion = app.web.drjackycv.buildsrc.Depends.Versions.detektVersion
+    buildUponDefaultConfig = true
+    baseline = baselineFile
+    config = files("config/detekt/detekt.yml")
+    input = objects.fileCollection().from(
+        DetektExtension.DEFAULT_SRC_DIR_JAVA,
+        "src/test/java",
+        DetektExtension.DEFAULT_SRC_DIR_KOTLIN,
+        "src/test/kotlin"
+    )
     reports {
         html.enabled = true
         html.destination = file("$projectDir/build/detekt/report.html")
@@ -20,72 +36,9 @@ detekt {
         txt.destination = file("$projectDir/build/detekt/report.txt")
     }
 }
+
 dependencies {
-    detektPlugins("io.gitlab.arturbosch.detekt:detekt-formatting:1.15.0-RC1")
-}
-
-
-/*plugins {
-    id("io.gitlab.arturbosch.detekt")
-}*/
-
-val analysisDir = file(projectDir)
-val baselineFile = file("$rootDir/config/detekt/baseline.xml")
-val configFile = file("$rootDir/config/detekt/detekt.yml")
-val statisticsConfigFile = file("$rootDir/config/detekt/statistics.yml")
-
-val kotlinFiles = "**/*.kt"
-val kotlinScriptFiles = "**/*.kts"
-val resourceFiles = "**/resources/**"
-val buildFiles = "**/build/**"
-
-/*
-subprojects {
-
-    apply {
-        plugin("io.gitlab.arturbosch.detekt")
-    }
-
-    tasks.withType<Detekt>().configureEach {
-        jvmTarget = "1.8"
-    }
-
-    detekt {
-        input = objects.fileCollection().from(
-            DetektExtension.DEFAULT_SRC_DIR_JAVA,
-            "src/test/java",
-            DetektExtension.DEFAULT_SRC_DIR_KOTLIN,
-            "src/test/kotlin"
-        )
-        buildUponDefaultConfig = true
-        baseline = baselineFile
-        config = files("config/detekt/detekt.yml")
-
-
-        reports {
-            html.enabled = true
-            html.destination = file("$projectDir/build/detekt/report.html")
-            xml.enabled = true
-            xml.destination = file("$projectDir/build/detekt/report.xml")
-            txt.enabled = true
-            txt.destination = file("$projectDir/build/detekt/report.txt")
-        }
-    }
-
-    dependencies {
-        detektPlugins("io.gitlab.arturbosch.detekt:detekt-formatting:1.15.0-RC1")
-    }
-}
-
-allprojects {
-
-    */
-/*dependencies {
-        detekt(project(":detekt-cli"))
-        detektPlugins(project(":custom-checks"))
-        detektPlugins(project(":detekt-formatting"))
-    }*//*
-
+    detektPlugins(app.web.drjackycv.buildsrc.Depends.ClassPaths.detekt_gradle_plugin)
 }
 
 val detektFormat by tasks.registering(Detekt::class) {
@@ -95,16 +48,16 @@ val detektFormat by tasks.registering(Detekt::class) {
     buildUponDefaultConfig = true
     autoCorrect = true
     setSource(analysisDir)
-    config.setFrom(listOf(statisticsConfigFile, configFile))
+//    config.setFrom(listOf(statisticsConfigFile, configFile))
     include(kotlinFiles)
     include(kotlinScriptFiles)
     exclude(resourceFiles)
     exclude(buildFiles)
     baseline.set(baselineFile)
     reports {
-        xml.enabled = false
-        html.enabled = false
-        txt.enabled = false
+        xml.enabled = true
+        html.enabled = true
+        txt.enabled = true
     }
 }
 
@@ -113,16 +66,16 @@ val detektAll by tasks.registering(Detekt::class) {
     parallel = true
     buildUponDefaultConfig = true
     setSource(analysisDir)
-    config.setFrom(listOf(statisticsConfigFile, configFile))
+//    config.setFrom(listOf(statisticsConfigFile, configFile))
     include(kotlinFiles)
     include(kotlinScriptFiles)
     exclude(resourceFiles)
     exclude(buildFiles)
     baseline.set(baselineFile)
     reports {
-        xml.enabled = false
-        html.enabled = false
-        txt.enabled = false
+        xml.enabled = true
+        html.enabled = true
+        txt.enabled = true
     }
 }
 
@@ -132,10 +85,10 @@ val detektProjectBaseline by tasks.registering(DetektCreateBaselineTask::class) 
     ignoreFailures.set(true)
     parallel.set(true)
     setSource(analysisDir)
-    config.setFrom(listOf(statisticsConfigFile, configFile))
+//    config.setFrom(listOf(statisticsConfigFile, configFile))
     include(kotlinFiles)
     include(kotlinScriptFiles)
     exclude(resourceFiles)
     exclude(buildFiles)
     baseline.set(baselineFile)
-}*/
+}
