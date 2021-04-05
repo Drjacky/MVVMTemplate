db3acadded41be63498c23c486f34213f41ca3bb
Dr.jacky <dr.jacky.2005@gmail.com>
2021-04-05 21:05:54 +0100

    - Ensures that the action lambda provided by `collectIn` mirrors that of `collect`. Namely, that it does not provide a `CoroutineScope` receiver, thus requiring developers be explicit if they want to create one. - Swaps out uses of the higher level `addRepeatingJob` for the lower level primitive of `repeatOnLifecycle`.

--- presentation/src/main/kotlin/app/web/drjackycv/presentation/extension/FlowExtension.kt
+++ presentation/src/main/kotlin/app/web/drjackycv/presentation/extension/FlowExtension.kt
@@ -2,10 +2,11 @@ package app.web.drjackycv.presentation.extension
 
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.LifecycleOwner
-import androidx.lifecycle.addRepeatingJob
-import kotlinx.coroutines.CoroutineScope
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.launch
 import kotlin.coroutines.CoroutineContext
 import kotlin.coroutines.EmptyCoroutineContext
 
@@ -13,9 +14,11 @@ inline fun <T> Flow<T>.collectIn(
     owner: LifecycleOwner,
     minActiveState: Lifecycle.State = Lifecycle.State.STARTED,
     coroutineContext: CoroutineContext = EmptyCoroutineContext,
-    crossinline action: suspend CoroutineScope.(T) -> Unit,
-) = owner.addRepeatingJob(minActiveState, coroutineContext) {
-    collect {
-        action(it)
+    crossinline action: suspend (T) -> Unit,
+) = owner.lifecycleScope.launch(coroutineContext) {
+    owner.lifecycle.repeatOnLifecycle(minActiveState) {
+        collect {
+            action(it)
+        }
     }
 }
