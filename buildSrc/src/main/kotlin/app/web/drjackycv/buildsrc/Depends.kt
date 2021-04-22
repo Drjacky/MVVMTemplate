fdb4ab7754e3d356dcb3aae3c1abe9c64db9b9e1
Dr.jacky <dr.jacky.2005@gmail.com>
2021-04-22 11:44:54 +0100

    Update gradle

--- buildSrc/src/main/kotlin/app/web/drjackycv/buildsrc/Depends.kt
+++ buildSrc/src/main/kotlin/app/web/drjackycv/buildsrc/Depends.kt
@@ -6,7 +6,7 @@ object Depends {
 
     object Versions {
         const val appVersionCode = 1_000_000
-        const val gradleVersion = "7.0.0-alpha12"
+        const val gradleVersion = "7.0.0-alpha14"
         const val androidCompileSdkVersion = 30
         const val targetSdkVersion = 30
         const val minSdkVersion = 21
