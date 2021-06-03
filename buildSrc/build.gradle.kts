657305334cbbc44af2b7344ebdd87f276662c064
Dr.jacky <dr.jacky.2005@gmail.com>
2021-06-03 16:33:29 +0100

    Remove manual added proguard dependency

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -9,7 +9,7 @@ repositories {
 
 dependencies {
     implementation("io.gitlab.arturbosch.detekt:detekt-gradle-plugin:1.15.0-RC1")
-    implementation("net.sf.proguard:proguard-gradle:6.2.2")
+    //implementation("net.sf.proguard:proguard-gradle:6.2.2")
 }
 
 kotlinDslPluginOptions {
