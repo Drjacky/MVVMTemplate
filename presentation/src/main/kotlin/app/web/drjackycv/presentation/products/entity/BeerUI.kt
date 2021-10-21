1f7d9192ad284cf93e263014e3d40f7fceebc5e7
Drjacky <dr.jacky.2005@gmail.com>
2021-10-21 15:05:16 +0100

    Delete adapter classes

--- presentation/src/main/kotlin/app/web/drjackycv/presentation/products/entity/BeerUI.kt
+++ presentation/src/main/kotlin/app/web/drjackycv/presentation/products/entity/BeerUI.kt
@@ -1,15 +1,14 @@
 package app.web.drjackycv.presentation.products.entity
 
 import android.os.Parcelable
-import app.web.drjackycv.presentation.base.adapter.RecyclerItem
 import kotlinx.parcelize.Parcelize
 
 @Parcelize
 data class BeerUI(
-    override val id: Int,
+    val id: Int,
     val name: String,
     val tagline: String,
     val description: String,
     val imageUrl: String?,
     val abv: Double
-) : RecyclerItem, Parcelable
+) : Parcelable
