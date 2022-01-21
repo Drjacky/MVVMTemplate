55c5cfd4f840d20a765c47aedcc590cb50f6fee4
Drjacky <dr.jacky.2005@gmail.com>
2022-01-21 15:16:56 +0000

    Migrate to kotlin 1.6.10 + Update dependencies

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -9,7 +9,7 @@ repositories {
 }
 
 dependencies {
-    implementation("org.jetbrains.kotlin.jvm:org.jetbrains.kotlin.jvm.gradle.plugin:1.6.0")
-    implementation("com.android.tools.build:gradle:7.0.3")
-    implementation("io.gitlab.arturbosch.detekt:detekt-gradle-plugin:1.18.1")
+    implementation("org.jetbrains.kotlin.jvm:org.jetbrains.kotlin.jvm.gradle.plugin:1.6.10")
+    implementation("com.android.tools.build:gradle:7.0.4")
+    implementation("io.gitlab.arturbosch.detekt:detekt-gradle-plugin:1.19.0")
 }
