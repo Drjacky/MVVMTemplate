ecdda95d1a9a5d4048e88315d012542649beddd7
Drjacky <dr.jacky.2005@gmail.com>
2022-04-05 15:46:28 +0100

    Update kotlin to 1.6.20 and update dependencies

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -9,7 +9,7 @@ repositories {
 }
 
 dependencies {
-    implementation("org.jetbrains.kotlin.jvm:org.jetbrains.kotlin.jvm.gradle.plugin:1.6.10")
+    implementation("org.jetbrains.kotlin.jvm:org.jetbrains.kotlin.jvm.gradle.plugin:1.6.20")
     implementation("com.android.tools.build:gradle:7.2.0-alpha07")
     implementation("io.gitlab.arturbosch.detekt:detekt-gradle-plugin:1.19.0")
 }
