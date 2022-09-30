61518b0786113bfdfc9e11ab9b2e29c4c1be89a9
Drjacky <dr.jacky.2005@gmail.com>
2022-09-30 14:38:51 +0100

    Replace deprecated onbackpress

--- presentation/src/main/kotlin/app/web/drjackycv/presentation/main/MainActivity.kt
+++ presentation/src/main/kotlin/app/web/drjackycv/presentation/main/MainActivity.kt
@@ -1,6 +1,7 @@
 package app.web.drjackycv.presentation.main
 
 import android.os.Bundle
+import androidx.activity.OnBackPressedCallback
 import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.app.AppCompatDelegate
@@ -67,22 +68,17 @@ class MainActivity : AppCompatActivity() {
                 })
         }
         setupUI()
+
+        onBackPressedDispatcher.addCallback(this, object : OnBackPressedCallback(true) {
+            override fun handleOnBackPressed() {
+                finish()
+            }
+        })
     }
 
     override fun onStop() {
         uiStateJob?.cancel()
         super.onStop()
-    }
-
-    override fun onBackPressed() {
-        if (isTaskRoot
-            && supportFragmentManager.primaryNavigationFragment?.childFragmentManager?.backStackEntryCount == 0
-            && supportFragmentManager.backStackEntryCount == 0
-        ) {
-            finishAfterTransition()
-        } else {
-            super.onBackPressed()
-        }
     }
 
     override fun onDestroy() {
