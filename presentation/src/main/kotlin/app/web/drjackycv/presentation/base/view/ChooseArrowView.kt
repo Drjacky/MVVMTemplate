cea5819229d42a51dc0bd7310ffe7b2b02f1a3d1
Drjacky <dr.jacky.2005@gmail.com>
2024-06-15 18:10:47 +0100

    Fix detekt import and magic number

--- presentation/src/main/kotlin/app/web/drjackycv/presentation/base/view/ChooseArrowView.kt
+++ presentation/src/main/kotlin/app/web/drjackycv/presentation/base/view/ChooseArrowView.kt
@@ -1,15 +1,31 @@
 package app.web.drjackycv.presentation.base.view
 
-import androidx.compose.animation.core.*
+import androidx.compose.animation.core.LinearEasing
+import androidx.compose.animation.core.RepeatMode
+import androidx.compose.animation.core.animateDpAsState
+import androidx.compose.animation.core.animateFloat
+import androidx.compose.animation.core.infiniteRepeatable
+import androidx.compose.animation.core.rememberInfiniteTransition
+import androidx.compose.animation.core.tween
 import androidx.compose.foundation.Canvas
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.absoluteOffset
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.requiredHeight
+import androidx.compose.foundation.layout.requiredWidth
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.rotate
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.geometry.Rect
-import androidx.compose.ui.graphics.*
+import androidx.compose.ui.graphics.Outline
+import androidx.compose.ui.graphics.Paint
+import androidx.compose.ui.graphics.PaintingStyle
+import androidx.compose.ui.graphics.Path
+import androidx.compose.ui.graphics.PathEffect
+import androidx.compose.ui.graphics.StrokeCap
+import androidx.compose.ui.graphics.drawOutline
 import androidx.compose.ui.graphics.drawscope.drawIntoCanvas
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.Dp
@@ -21,12 +37,14 @@ import app.web.drjackycv.presentation.extension.percentOf
 import app.web.drjackycv.presentation.products.choose.CirclePosition
 
 
+@Suppress("MagicNumber")
 @Composable
 fun ChooseArrowAnimation(
     circleState: CirclePosition,
     modifier: Modifier = Modifier,
 ) {
     val offsetAnimation: Dp by animateDpAsState(
+        label = "offsetAnimation",
         targetValue = if (circleState == CirclePosition.Start) 0.dp else 100.dp,
         animationSpec = infiniteRepeatable(
             animation = tween(
@@ -36,8 +54,9 @@ fun ChooseArrowAnimation(
             repeatMode = RepeatMode.Reverse
         ),
     )
-    val infiniteTransition = rememberInfiniteTransition()
+    val infiniteTransition = rememberInfiniteTransition(label = "infiniteTransition")
     val angle by infiniteTransition.animateFloat(
+        label = "angleAnimation",
         initialValue = 0F,
         targetValue = 180F,
         animationSpec = infiniteRepeatable(
