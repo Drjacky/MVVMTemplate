8cddc3d5bab5a71a42eb82a5215e62ad389e2824
Drjacky <dr.jacky.2005@gmail.com>
2026-03-11 17:42:50 +0000

    [WIP] Remove unused observe extension function from Extensions.kt

--- feature/products/src/main/kotlin/app/web/drjackycv/feature/products/extension/Extensions.kt
+++ feature/products/src/main/kotlin/app/web/drjackycv/feature/products/extension/Extensions.kt
@@ -6,15 +6,10 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.DefaultLifecycleObserver
 import androidx.lifecycle.LifecycleOwner
-import androidx.lifecycle.LiveData
 import androidx.lifecycle.Observer
 import androidx.viewbinding.ViewBinding
 import kotlin.properties.ReadOnlyProperty
 import kotlin.reflect.KProperty
-
-fun <T> LifecycleOwner.observe(liveData: LiveData<T>, action: (t: T) -> Unit) {
-    liveData.observe(this, Observer { it?.let { t -> action(t) } })
-}
 
 class FragmentViewBindingDelegate<T : ViewBinding>(
     val fragment: Fragment,
